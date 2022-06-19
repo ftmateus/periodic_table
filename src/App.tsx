452f76ff3fef9ed6c08645f462b860e13b395697
Francisco Mateus <ft.mateus@campus.fct.unl.pt>
2022-06-19 15:50:44 +0100

    Filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton, Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import OffcanvasHeader from 'react-bootstrap/OffcanvasHeader'
 import Offcanvas from 'react-bootstrap/Offcanvas'
@@ -25,13 +25,22 @@ type Element =
 	atomic_mass : number;
 }
 
+enum Filters
+{
+	RADIOACTIVITY = "Radioactivity",
+	TYPE = "Type",
+	BLOCK = "Block",
+	OCCURENCE = "Occurence"
+}
+
 type ElementContainerProps = 
 {
  	element : Element;
-	setCurrentElement : Dispatch<SetStateAction<Element | undefined>>
+	setCurrentElement : Dispatch<SetStateAction<Element | undefined>>;
+	currentFilter : Filters
 }
 
-function ElementContainer({element, setCurrentElement} : ElementContainerProps)
+function ElementContainer({element, setCurrentElement, currentFilter} : ElementContainerProps)
 {
 	console.assert(element != undefined)
 
@@ -43,21 +52,61 @@ function ElementContainer({element, setCurrentElement} : ElementContainerProps)
 	}
   
 
-	let color = "rgb(255, 153, 153)";
-	{
+	let color = undefined;
+	if(currentFilter == Filters.BLOCK){
 		if(element.block == "P") color = "rgb(253, 255, 140)"
 		// else if(element.block == "D") color = "deepskyblue"
 		else if(element.block == "D") color = "rgb(153, 204, 255)"
 		else if(element.block == "F") color = "rgb(155, 255, 153)"
-		else console.assert(element.block == "S")
+		else {
+			color = "rgb(255, 153, 153)"
+			console.assert(element.block == "S")
+		}
 	}
+	else if(currentFilter == Filters.TYPE)
+	{
+		if(element.type == "Nonmetal") color = "rgb(253, 255, 140)"
+		// else if(element.block == "D") color = "deepskyblue"
+		else if(element.type == "Metal") color = "lightgrey"
+		else {
+			color = "rgb(255, 153, 153)"
+			console.assert(element.type == "Semimetal")
+		}
+	}
+	else if(currentFilter == Filters.OCCURENCE)
+	{
+		if(element.occurence == "Primordial") color = "rgb(155, 255, 153)"
+		else if(element.occurence == "From Decay") color = "rgb(255, 153, 153)"
+		else 
+		{
+			console.assert(element.occurence == "Synthetic")
+			color = "deepskyblue"
+		} 
+	}
+	else 
+	{
+		console.assert(currentFilter == Filters.RADIOACTIVITY)
+		if(element.radioactive) color = "red"
+		else color = "green"
+	}
+	// {
+	// 	color = "red";
+	// 	if(element.radioactive) color = "red"
+	// 	else color = "green"
+	// }
 
 
+	// let component = <></>
+
+	// if(element.atomic_number == 57) //Lanthanum
+	// {
+
+	// }
 	return <div 
 		className="element_container_out"
 		style={{
 			gridRowStart: element.block == "F" ? 8 + (element.element_period - 5) : element.element_period,  
-			gridColumnStart: element.block == "F" ? -element.element_group + 2: element.element_group
+			gridColumnStart: element.block == "F" ? -element.element_group + 3 : element.element_group > 2 ? element.element_group + 1 : element.element_group
 		}}
 	>
 		<div id={element.symbol} key={element.atomic_number} className="element_container" 
@@ -84,15 +133,26 @@ function ElementContainer({element, setCurrentElement} : ElementContainerProps)
 type MainTableProps =
 {
 	setCurrentElement : Dispatch<SetStateAction<Element | undefined>>
+	currentFilter : Filters
 }
 
-function MainTable({setCurrentElement} : MainTableProps)
+function MainTable({setCurrentElement, currentFilter} : MainTableProps)
 {
 
     let table = <div className="table_container">
       {
         elements.map((element, _) =>
-          <ElementContainer element={element} setCurrentElement={setCurrentElement}/>
+		{
+			
+			return <>	
+				<ElementContainer 
+					element={element} 
+					setCurrentElement={setCurrentElement} 
+					currentFilter={currentFilter}
+				/>
+			</>
+
+		}
         )
       }
     </div>
@@ -106,11 +166,17 @@ function App() {
 
 	const [show, setShow] = useState(false);
 	const [currentElement, setCurrentElement] = useState<Element | undefined>(undefined);
+	const [currentFilter, setCurrentFilter] = useState(Filters.BLOCK);
+	const [loadingWikipedia, setLoadingWikipedia] = useState(false);
 
 	React.useEffect(()=>{
 		// console.log(currentElement)
 		if(currentElement)
+		{
 			setShow(true);
+			setLoadingWikipedia(true)
+		}
+			
 	}, [currentElement]);
 
 	const handleClose = () => {
@@ -121,26 +187,51 @@ function App() {
 
 	return (
 		<div style={{position: "relative"}}>
-		<MainTable setCurrentElement={setCurrentElement}/>
+		<MainTable setCurrentElement={setCurrentElement} currentFilter={currentFilter}/>
 		
 		<div className="legend_container" id="legend_container">
-			<h3 className="occurence_legend" style={{borderStyle: "solid"}}>Primordial</h3>
-			<h3 className="occurence_legend" style={{borderStyle: "dashed"}}>Decay</h3>
-			<h3 className="occurence_legend" style={{borderStyle: "dotted"}}>Synthetic</h3>
+			<div>
+				<h3 className="occurence_legend" style={{borderStyle: "solid"}}>Primordial</h3>
+				<h3 className="occurence_legend" style={{borderStyle: "dashed"}}>Decay</h3>
+				<h3 className="occurence_legend" style={{borderStyle: "dotted"}}>Synthetic</h3>
+			</div>
+			<div>
+				<DropdownButton
+					align={{ lg: 'end' }}
+					title={`Filter (${currentFilter}) `}
+					id="filter"
+				>
+					<Dropdown.Item eventKey="1" onClick={() => setCurrentFilter(Filters.RADIOACTIVITY)}>Radioactivity</Dropdown.Item>
+					<Dropdown.Item eventKey="2" onClick={() => setCurrentFilter(Filters.TYPE)}>Type</Dropdown.Item>
+					<Dropdown.Item eventKey="3" onClick={() => setCurrentFilter(Filters.OCCURENCE)}>Occurence</Dropdown.Item>
+					<Dropdown.Item eventKey="4" onClick={() => setCurrentFilter(Filters.BLOCK)}>Block (Default)</Dropdown.Item>
+    			</DropdownButton>
+			</div>
 		</div>
 
 		<Offcanvas show={show} onHide={handleClose} placement="end" style={{margin : 0, width : 600}}>
 			<Offcanvas.Header closeButton>
-			<Offcanvas.Title>{currentElement?.name ?? "Select an element first!"}</Offcanvas.Title>
-			<Button variant="danger">
-				Wikipedia
-			</Button>
-			<Button variant="primary">
-				Google
-			</Button>
-			<Button variant="primary">
-				Bing
-			</Button>
+			<Offcanvas.Title>
+				{currentElement?.name ?? "Select an element first!"}
+				{
+					loadingWikipedia ?
+					<Spinner animation="border" role="status">
+						<span className="visually-hidden">Loading...</span>
+					</Spinner>
+					: <></>
+				}
+			</Offcanvas.Title>
+			<div style={{display : "flex", justifyContent : "space-between", width : 300}}>
+				<Button variant="danger">
+					Wikipedia
+				</Button>
+				<Button variant="primary">
+					Google
+				</Button>
+				<Button variant="primary">
+					Bing
+				</Button>
+			</div>
 			</Offcanvas.Header>
 			<Offcanvas.Body style={{padding : 0, overflow: "hidden"}}>
 				<iframe 
@@ -148,6 +239,7 @@ function App() {
 					style={{padding : 0}} 
 					className="element_properties_frame" 
 					src={currentElement ? "https://en.m.wikipedia.org/wiki/" + currentElement.name : ""} 
+					onLoad={() => setLoadingWikipedia(false) }
 				/>
 			</Offcanvas.Body>
 		</Offcanvas>
